a565fca402eb75436639a218f29805f547e42523
Allen Garvey <allen@allengarvey.com>
2016-10-10 21:26:15 -0400

    Add function to folder model to retrieve list of all folders in format suitable for select field in forms

--- web/models/folder.ex
+++ web/models/folder.ex
@@ -31,4 +31,13 @@ defmodule Bookmarker.Folder do
   def all_in_order_query() do
     from(f in Bookmarker.Folder, order_by: f.name)
   end 
+
+  @doc """
+  Returns list of folders with name and id
+  ordered in default order suitable for select fields
+  for forms
+  """
+  def form_list(repo) do
+    repo.all(Bookmarker.Folder.all_in_order_query()) |> Enum.map(&{&1.name, &1.id})
+  end
 end
