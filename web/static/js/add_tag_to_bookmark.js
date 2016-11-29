e8be0adf23188fec14b3791d4620bae6d75bafd5
Allen Garvey <allen.garvey@gmail.com>
2016-11-29 11:35:08 -0500

    Complete functionality to add and remove tags to bookmarks on show page using ajax

--- web/static/js/add_tag_to_bookmark.js
+++ web/static/js/add_tag_to_bookmark.js
@@ -2,48 +2,78 @@
 
 var AddTagToBookmark = {};
 AddTagToBookmark.init = function($){
+	//initialize variables
+	var bookmarkId = $('[data-bookmark-id]').data('bookmark-id');
+	var tagOptionTemplate = $.template($('#add_tag_option_template').html());
+	var tagSelectTag = $('.add-tag-container select');
+	var tagListItemTemplate = $.template($('#add_tag_tag_item_template').html());
+	var tagList = $('.tag-list');
+
+	//populate and show tag select form
 	$('[data-role="add-tag-button"]').on('click', function(e){
 		$(this).hide();
 
 		$.ajax({
-			url: '/api/tags/bookmark/1/unused',
+			url: '/api/tags/bookmark/' + bookmarkId + '/unused',
 			type: 'GET',
 			dataType: 'json',
-			success: function(data){
-				if(data.error){
-					console.log(data);
+			success: function(response){
+				if(response.error){
 					return;
 				}
-				var tagOptionTemplate = $.template($('#add_tag_option_template').html());
-				var tagSelectTag = $('.add-tag-container select');
 				tagSelectTag.html('');
-				data.data.forEach(function(tag){
-						tagSelectTag.append($.parseHTML(tagOptionTemplate.render({id: tag.id, name: tag.attributes.name})));
+				response.data.forEach(function(tag){
+						tagSelectTag.append(tagOptionTemplate.render({id: tag.id, name: tag.attributes.name}));
 					}
 				);
 				$('.add-tag-container').show();
 			}
 		});
 	});
+	//reset tag select and add button
 	function resetAddTagForm(){
 		$('[data-role="add-tag-button"]').show();
 		$('.add-tag-container').hide();
 	}
 
+	//cancel adding tag
 	$('[data-role="cancel-tag-button"]').on('click', function(event) {
 		resetAddTagForm();
 	});
 
+	//save tag
 	$('[data-role="save-tag-button"]').on('click', function(event){
-		var bookmarkId = 1;
 		var tagId = $('.add-tag-container select').val();
 		$.ajax({
 			url: '/api/bookmarks_tags/',
 			method: 'POST',
+			dataType: 'json',
 			data: {bookmark_id: bookmarkId, tag_id: tagId},
-			success: function(data){
-				console.log(data);
+			success: function(response){
 				resetAddTagForm();
+				if(response.error){
+					return;
+				}
+				tagList.append(tagListItemTemplate.render({tagId: response.data.attributes.tag.id, tagName: response.data.attributes.tag.attributes.name}));
+			}
+		});
+		
+	});
+
+	//remove tag
+	$('.tag-list').on('click', '[data-role="remove-tag-button"]', function(event) {
+		var removeButton = $(this);
+		var tagId = removeButton.closest('[data-tag-id]').data('tag-id');
+		$.ajax({
+			url: '/api/bookmarks_tags/',
+			method: 'DELETE',
+			dataType: 'json',
+			data: {bookmark_id: bookmarkId, tag_id: tagId},
+			success: function(response){
+				if(response.error){
+					return;
+				}
+				removeButton.closest('.list-group-item').remove();
 			}
 		});
 		
