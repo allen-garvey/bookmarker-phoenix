fac0ace7758eaee0afe758497b5af581159c262c
Allen Garvey <allen.garvey@gmail.com>
2018-01-01 12:08:07 -0500

    Update phoenix and related dependencies

--- mix.exs
+++ mix.exs
@@ -31,16 +31,16 @@ defmodule Bookmarker.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.1"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
+      {:phoenix_ecto, "~> 3.3"},
+      {:postgrex, "~> 0.13"},
+      {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
+      {:gettext, "~> 0.14"},
       {:cowboy, "~> 1.0"},
       {:httpoison, "~> 0.13"}, #for folder previews
-      {:floki, "~> 0.18.0"},   #for folder previews
+      {:floki, "~> 0.19"},   #for folder previews
    ]
   end
 
